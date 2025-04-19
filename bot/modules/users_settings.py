ceccec54c2fb26172e50e5223d403c07784d0b02
searchingshiv <108224889+searchingshiv@users.noreply.github.com>
2025-04-20 00:06:58 +0530

    Update users_settings.py

--- bot/modules/users_settings.py
+++ bot/modules/users_settings.py
@@ -666,13 +666,15 @@ async def get_user_settings(from_user, key=None, edit_type=None, edit_mode=None)
     return text, button
 
 
-async def update_user_settings(
-    query, key=None, edit_type=None, edit_mode=None, msg=None, sdirect=False
-):
+async def update_user_settings(query, key=None, edit_type=None, edit_mode=None, msg=None, sdirect=False):
     msg, button = await get_user_settings(
-        msg.from_user if sdirect else query.from_user, key, edit_type, edit_mode
+        msg.from_user if sdirect else query.from_user,
+        key,
+        edit_type,
+        edit_mode
     )
     await editMessage(query if sdirect else query.message, msg, button)
+
 
 
 async def user_settings(client, message):
